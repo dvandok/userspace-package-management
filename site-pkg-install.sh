f23e85177417d6af4a502409f9d2c28f7f1ecc97
Mark Santcroos <marksant@grid-ui01.amc.nl>
2010-09-01 18:42:48 +0200

    * Delegation based on username * Setting for output job ids * Fix resource selection (Did that even work earlier?) * Enable WMS perusal * Cleanup JDL

--- site-pkg-install.sh
+++ site-pkg-install.sh
@@ -4,19 +4,19 @@
 
 # Site is first argument
 # the software is the remainder
-delegation=dennis
+
+JIDS=site-pkg.jids
 
 site=$1
 
 shift
 
-resource=`awk '$1='$site' { print $2 }`
+resource=`awk '$1=="'$site'" { print $2 }' resources.dat`
 
-if [ -z $resource ] ; then
+if [ -z "$resource" ]; then
     echo "Resource for site '$site' not found in resources.dat" > /dev/stderr
     exit 1
 fi
-
 
 if [ $# -lt 1 ] ; then
     echo "Usage: $0 <site> package [ package ... ]" > /dev/stderr
@@ -29,17 +29,22 @@ Executable = "pkgsrc-cmd.sh";
 Arguments = "install $@";
 Stdoutput = "stdout";
 StdError = "stderror";
-InputSandbox = "../pkgsrc-cmd.sh";
+InputSandbox = "pkgsrc-cmd.sh";
 OutputSandbox = {"stdout","stderror"};
+PerusalFileEnable = true;
+PerusalTimeInterval = 60;
 EOF
 
-glite-wms-job-submit -d $delegation -o jids -r $resource $jdl
+glite-wms-job-submit -d $USER -o $JIDS -r $resource $jdl
 if [ $? -ne 0 ]; then
     echo "Failed to submit job $jdl to $resource" > /dev/stderr
     exit 1
 fi
 
+
+# enabling perusal for stdout and stderr
+glite-wms-job-perusal --set -f stdout -f stderror `tail -1 $JIDS`
 echo "Job submitted; check back later for results."
 
+rm $jdl
 exit 0
-
