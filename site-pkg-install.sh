f04b86cad6b3fe92f63119e0c288d63d97dc3808
Mark Santcroos <marksant@grid-ui01.amc.nl>
2010-09-01 18:51:45 +0200

    * Change into a generic tool that supports all commands that pkgsrc-cmd.sh also supports

--- site-pkg-install.sh
+++ site-pkg-install.sh
@@ -5,28 +5,65 @@
 # Site is first argument
 # the software is the remainder
 
-JIDS=site-pkg.jids
+#
+function usage {
+	echo "Usage: $0 <site> <install | remove | update> [ package ... ]" > /dev/stderr
+	echo "       $0 <site> <init | reinit | check | version>" > /dev/stderr
+}
+
+JIDS=site-pkgtool.jids
 
 site=$1
-
 shift
+
+if [ -z "$site" ]; then
+	usage
+	exit 1
+fi
 
 resource=`awk '$1=="'$site'" { print $2 }' resources.dat`
 
 if [ -z "$resource" ]; then
-    echo "Resource for site '$site' not found in resources.dat" > /dev/stderr
-    exit 1
+	echo "Site '$site' not found in resources.dat." > /dev/stderr
+	usage
+	exit 1
 fi
 
-if [ $# -lt 1 ] ; then
-    echo "Usage: $0 <site> package [ package ... ]" > /dev/stderr
-    exit 1
+if [ $# -gt 0 ]; then
+    case $1 in
+        init | reinit | check | version)
+		command=$1
+    		shift
+		if [ $# -gt 0 ]; then
+			usage;
+			exit 1
+		fi
+		;;
+        install | remove | update)
+		command=$1
+    		shift
+		if [ $# -le 0 ]; then
+			echo "No package(s) given."
+			usage;
+			exit 1
+		fi
+		;;
+        *)
+		echo "Unknown command given."
+	    usage;
+            exit 1
+            ;;
+    esac
+else
+	echo "No command given."
+	usage
+	exit 1
 fi
 
-jdl=`mktemp site-install.jdl-XXXXXXXXXX`
+jdl=`mktemp site-pkg.jdl-XXXXXXXXXX`
 cat > $jdl <<EOF
 Executable = "pkgsrc-cmd.sh";
-Arguments = "install $@";
+Arguments = "$command $@";
 Stdoutput = "stdout";
 StdError = "stderror";
 InputSandbox = "pkgsrc-cmd.sh";
@@ -40,7 +77,6 @@ if [ $? -ne 0 ]; then
     echo "Failed to submit job $jdl to $resource" > /dev/stderr
     exit 1
 fi
-
 
 # enabling perusal for stdout and stderr
 glite-wms-job-perusal --set -f stdout -f stderror `tail -1 $JIDS`
